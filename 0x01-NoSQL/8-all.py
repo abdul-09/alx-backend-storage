23740c4e3567525297aac66615f3e7c72b1228db
abdul-09 <abdul.9.hussein@gmail.com>
2024-01-23 17:19:50 +0300

    updated

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+"""Write a Python function that lists all documents in a collection:
+
+Prototype: def list_all(mongo_collection):
+Return an empty list if no document in the collection
+mongo_collection will be the pymongo collection object
+"""
+
+
+import pymongo
 
 
 def list_all(mongo_collection):
-    """ List all documents in Python """
-    documents = mongo_collection.find()
-
-    if documents.count() == 0:
+    """Return list of all docs in collection"""
+    if not mongo_collection:
         return []
-
-    return documents
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
